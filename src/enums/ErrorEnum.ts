42d0eed5faaa4cf7785ab5132b9492cff4567997
Vinicius Smanioto <smaniotov@gmail.com>
2019-08-12 03:44:00 -0300

    add new errors codes to enum

--- src/enums/ErrorEnum.ts
+++ src/enums/ErrorEnum.ts
@@ -5,7 +5,10 @@ enum ErrorEnum {
   Forbidden = 'FORBIDDEN',
   PersistedQueryNotSupported = 'PERSISTED_QUERY_NOT_SUPPORTED',
   InputError = 'BAD_USER_INPUT',
-  InternalServerError = 'INTERNAL_SERVER_ERROR'
+  InternalServerError = 'INTERNAL_SERVER_ERROR',
+  Conflict = 'CONFLICT',
+  GraphqlValidationFailed = 'GRAPHQL_VALIDATION_FAILED',
+  BadUserInput = 'BAD_USER_INPUT'
 }
 
 export default ErrorEnum;
